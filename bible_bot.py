ce44372a2d199743dcc3f5fdf9fea876640b09f0
Shep <shepardjberry@gmail.com>
2020-08-05 21:00:57 -0500

    Added features such as try/excepts and passages

---
+++ bible_bot.py
@@ -0,0 +1,8 @@
+#BIBLE BOT
+
+import discord
+import itertools
+from discord.ext import commands
+import discord.utils
+
+from bible_lib import Bible
--- bible_lib.py
+++ bible_lib.py
@@ -1,6 +1,17 @@
 #Bible library
 import requests 
 
+def main():
+    bible = Bible(api_key='9ae9ceb9738443901b5af8767640e8c8')
+
+    verse = input("Input a bible verse")
+
+    book, verse = verse.split(" ")
+    chapter, verse = verse.split(":")
+    print(book, chapter, verse)
+
+    print(bible.get_verses(book_name=book, chapter=chapter, verse=verse))
+    
 class Bible:
     known_bibles = {"KJV": "de4e12af7f28f599-02", "DRA": "179568874c45066f-01"}
 
@@ -8,15 +19,39 @@ class Bible:
         self.api = BibleAPI(api_key=api_key)
         self.bible_id = bible_id
 
-    def get_verse(self, book_name="", chapter="", verse=""):
-        reference = f"{book_name} {chapter}:{verse}"
-        book_id = self.api.get_book_names(self.bible_id)[book_name]
-        chapter_id = self.api.get_chapter_names(bible_id=self.bible_id, book_id=book_id)[chapter]
-        verse_id = self.api.get_verse_names(bible_id=self.bible_id, chapter_id=chapter_id)[reference]
-        verse = self.api.get_verse(bible_id=self.bible_id, verse_id=verse_id)["data"]
-        return verse["content"]
-
+    def get_verses(self, book_name="", chapter="", verse=""):
+        try:
+            book_id = self.api.get_book_names(self.bible_id)[book_name.lower()]
+        except KeyError:
+            return ("Book not found", 404)
+        
     
+        try:
+            chapter_id = self.api.get_chapter_names(bible_id=self.bible_id, book_id=book_id)[chapter]
+        except KeyError:
+            return ("Chapter not found", 404)
+        if "-" not in verse:
+            reference = f"{book_name} {chapter}:{verse}"
+            try:
+                verse_id = self.api.get_verse_names(bible_id=self.bible_id, chapter_id=chapter_id)[reference]
+                verse = self.api.get_verse(bible_id=self.bible_id, verse_id=verse_id)["data"]
+            except: 
+                return ("Verse not found", 404)
+
+            return (verse["content"], 200)
+
+        verse_nums = verse.split("-")
+        try:
+            passage_id = f'{chapter_id}.{verse_nums[0]}-{chapter_id}.{verse_nums[1]}'
+
+            passage = self.api.get_passage(bible_id=self.bible_id, passage_id=passage_id)["data"]
+        except: 
+            return ("Passage not found", 404)
+        return (passage["content"], 200)
+        
+    def is_book(self, book_name):
+        return book_name.lower() in self.api.get_book_names(self.bible_id).keys()
+
 class BibleAPI:
 
     def __init__(self, api_key=""):
@@ -36,7 +71,7 @@ class BibleAPI:
     def get_book_names(self, bible_id=""):
         books = dict() 
         for book in self.get_books(bible_id=bible_id)['data']:
-            books[book.get("name")] = book.get("id")
+            books[book.get("name").lower()] = book.get("id")
         return books
 
     def get_chapters(self, bible_id="", book_id=""):
@@ -59,3 +94,7 @@ class BibleAPI:
         for verse in self.get_verses(bible_id=bible_id, chapter_id=chapter_id)['data']:
             verses[verse.get("reference")] = verse.get("id")
         return verses
+
+    def get_passage(self, bible_id="", passage_id="", params={"content-type": "text", "include-verse-numbers": True}):
+        return requests.get(f'https://api.scripture.api.bible/v1/bibles/{bible_id}/passages/{passage_id}', params=params, headers=self.headers).json()
+main()
